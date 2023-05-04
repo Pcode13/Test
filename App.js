e91aab854d17ba4b636d90d273ec688da4f6f5fc
Pcode13 <veer.pragati99@gmail.com>
2023-05-04 00:40:34 -0700

    Unique code

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AllNavigation from './src/Navigation/AllNavigation';
+
+
+
+const App = () =>{
+    return (
+        <Text>React Native App</Text>
+    )
+}
+  
+
+export default App;
